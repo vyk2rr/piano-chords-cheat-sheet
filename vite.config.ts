fc71dd738de9f1c7aa1e16b31b74ece5438a2a2d
Victor De la Rocha <vyk2rr@gmail.com>
2025-07-05 17:36:10 -0600

    chore: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'; 
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/piano-chords-cheat-sheet/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+});
+
